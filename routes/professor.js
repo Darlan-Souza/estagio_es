b9df438cf9e057935edf2819fe881a24528726b3
Darlan <darlan.sgf@gmail.com>
2019-06-22 10:41:48 -0300

    edicao de menu e botoes

--- routes/professor.js
+++ routes/professor.js
@@ -13,5 +13,17 @@ router.get('/registro_professor', function (req, res) {
     res.render("professor/registro_professor")
 })
 
+
+router.get("/login", function (req, res) {
+    res.render("professor/login")
+})
+
+//usuario fazer logout do sistema 
+router.get("/logout", function(req, res){
+    req.logout()
+    req.flash("success_msg", "Deslogado com sucesso")
+    res.redirect("/")
+})
+
 //Sempre fica por ultimo
 module.exports = router 
